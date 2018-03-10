97097c963edeecfc32d5c8de2af5bd227bbd46d7
A.Radmanesh <radmanesh@gmail.com>
2018-03-10 10:20:17 +0330

    added more pages

--- src/js/views/forms.js
+++ src/js/views/forms.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
   'use strict';
 
   $("#date").mask("99/99/9999");
@@ -9,7 +9,33 @@ $(function(){
     height: 300
   });
 
-  $('form.jvalidate').validate();
+  $.validator.setDefaults({
+    submitHandler: function() {
+      alert('submitted!');
+    }
+  });
+  $(document).ready(function(){
+    $("form.jvalidate").validate({
+      errorElement: 'em',
+      errorPlacement: function ( error, element ) {
+        // Add the `help-block` class to the error element
+        error.addClass( 'invalid-feedback' );
+        if ( element.prop( 'type' ) === 'checkbox' ) {
+          error.insertAfter( element.parent( 'label' ) );
+        } else {
+          error.insertAfter( element );
+        }
+      },
+      highlight: function ( element, errorClass, validClass ) {
+        $( element ).addClass( 'is-invalid' ).removeClass( 'is-valid' );
+        $( element ).parents( '.form-group' ).addClass( 'has-danger' ).removeClass( 'has-success' );
+      },
+      unhighlight: function (element, errorClass, validClass) {
+        $( element ).addClass( 'is-valid' ).removeClass( 'is-invalid' );
+        $( element ).parents( '.form-group' ).addClass( 'has-success' ).removeClass( 'has-danger' );
+      }
+    });
+  });
 
 
 })
