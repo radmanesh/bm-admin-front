39e037e4fcdcf7c26602d45039e6631fb3c4c081
A.Radmanesh <radmanesh@gmail.com>
2018-04-24 18:45:33 +0430

    Successfully migrated some views to new framework

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootstrap 4 BongaMonga admin front-end template
+using npm install then gulp <task>(e.g. dev)
--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,152 @@
-'use strict'
-
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin')
-var rename = require('gulp-rename');
-var runSequence = require('run-sequence');
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pug = require('gulp-pug');
+var beautify = require('gulp-html-beautify');
+var pkg = require('./package.json');
+var browserSync = require('browser-sync').create();
+// Set the banner content
+var banner = ['/*!\n',
+  ' * BongaMonga admin frontend  - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/radmanesh/<%= pkg.repoName %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function() {
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/**/*'
+    ])
+    .pipe(gulp.dest('./vendor/bootstrap'))
+  // DataTables
+  gulp.src([
+      './node_modules/datatables.net/js/*.js',
+      './node_modules/datatables.net-bs4/**/*',
+      './node_modules/datatables.net-responsive/js/*.js',
+      './node_modules/datatables.net-responsive-bs4/**/*'
+    ])
+    .pipe(gulp.dest('./vendor/datatables/'))
+  // Font Awesome
+  gulp.src([
+      './node_modules/font-awesome/**/*',
+      '!./node_modules/font-awesome/{less,less/*}',
+      '!./node_modules/font-awesome/{scss,scss/*}',
+      '!./node_modules/font-awesome/.*',
+      '!./node_modules/font-awesome/*.{txt,json,md}'
+    ])
+    .pipe(gulp.dest('./vendor/font-awesome'))
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
+  // Chart.js
+  gulp.src([
+      './node_modules/chart.js/dist/*',
+      '!./node_modules/chart.js/dist/docs/*'
+    ])
+    .pipe(gulp.dest('./vendor/chart.js'))
+  // jQuery Easing
+  gulp.src([
+      './node_modules/jquery.easing/*.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery-easing'))
+  // Popper
+  gulp.src([
+      './node_modules/popper.js/dist/*.js'
+    ])
+    .pipe(gulp.dest('./vendor/popper.js'))
+  // Select2
+  gulp.src([
+      './node_modules/select2/dist/js/**/*.js',
+      './node_modules/select2/dist/css/*.css'
+    ])
+    .pipe(gulp.dest('./vendor/select2/'))
+  // SummerNote
+  gulp.src([
+      './node_modules/summernote/dist/**/*'
+    ])
+    .pipe(gulp.dest('./vendor/summernote/'))
+  // jquery.inputmasked
+  gulp.src([
+      './node_modules/jquery.maskedinput/src/*.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery.maskedinput/'))
+  // jquery.validatie
+  gulp.src([
+      './node_modules/jquery-validation/dist/**/*'
+    ])
+    .pipe(gulp.dest('./vendor/jquery-validation/'))
 
-gulp.paths = {
-  dist: 'dist/',
-  src: 'src/',
-  vendors: 'dist/vendors/'
-};
-
-var paths = gulp.paths;
-
-gulp.pkg = require('./package.json');
-var pkg = gulp.pkg;
-
-require('require-dir')('./gulp-tasks');
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
+});
+// Compile SCSS
+gulp.task('css:compile', function() {
+  return gulp.src(['./scss/**/*.scss','!./scss/core/core.scss'])
+    .pipe(sass.sync({
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+});
+// Minify CSS
+gulp.task('css:minify', ['css:compile'], function() {
+  return gulp.src([
+      './css/*.css',
+      '!./css/*.min.css'
+    ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+// CSS
+gulp.task('css', ['css:compile', 'css:minify']);
+// Minify JavaScript
+gulp.task('js:minify', function() {
+  return gulp.src([
+      './js/*.js',
+      '!./js/*.min.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+});
+// JS
+gulp.task('js', ['js:minify']);
+// PUG
+gulp.task('pug', function buildHTML() {
+  return gulp.src('./pug/*.pug')
+    .pipe(pug())
+    .pipe(beautify())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+// Default task
+gulp.task('default', ['css', 'js', 'vendor']);
+// Configure the browserSync task
+gulp.task('browserSync', function() {
   browserSync.init({
-    server: ['./', './src']
-  });
-
-  gulp.watch(paths.src + 'scss/**/*.scss', ['sass']);
-  gulp.watch(paths.src + '**/*.html').on('change', browserSync.reload);
-  gulp.watch(paths.src + 'js/**/*.js').on('change', browserSync.reload);
-
-});
-
-// Static Server without watching scss files
-gulp.task('serve:lite', function() {
-
-  browserSync.init({
-    server: ['./', './src']
-  });
-
-  gulp.watch(paths.src + '**/*.css').on('change', browserSync.reload);
-  gulp.watch(paths.src + '**/*.html').on('change', browserSync.reload);
-  gulp.watch(paths.src + 'js/**/*.js').on('change', browserSync.reload);
-
-});
-
-gulp.task('serve:dist', function() {
-  browserSync.init({
-    server: ['./dist']
+    server: {
+      baseDir: "./"
+    }
   });
 });
-
-gulp.task('sass', ['compile-vendors'], function() {
-  return gulp.src(paths.src + '/scss/style.scss')
-  .pipe(sass())
-  .pipe(autoprefixer())
-  .pipe(gulp.dest(paths.src + 'css'))
-  .pipe(cssmin())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest(paths.src + 'css'))
-  .pipe(browserSync.stream());
+// Dev task
+gulp.task('dev', ['css', 'js', 'pug', 'browserSync'], function() {
+  gulp.watch('./pug/**/*', ['pug']);
+  gulp.watch('./scss/**/*.scss', ['css']);
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./*.html', browserSync.reload);
 });
-
-gulp.task('sass:watch', function() {
-  gulp.watch(paths.src + 'scss/**/*.scss', ['sass']);
-});
-
-gulp.task('default', ['serve']);
